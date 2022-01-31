514a58988f26cf65737362378d20081e8690fb43
abondar24 <abondar1992@gmail.com>
2022-01-31 12:38:03 +0100

    project base

---
+++ src/test/java/org/abondar/experimental/PhonebookTest.java
@@ -0,0 +1,21 @@
+package org.abondar.experimental;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class PhonebookTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
