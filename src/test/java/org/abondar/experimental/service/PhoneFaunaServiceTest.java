78e32fb33239aefd92200ae62498ed52c3d024d3
abondar24 <abondar1992@gmail.com>
2022-02-02 20:33:22 +0100

    added phone fauna service test

---
+++ src/test/java/org/abondar/experimental/service/PhoneFaunaServiceTest.java
@@ -0,0 +1,68 @@
+package org.abondar.experimental.service;
+
+import io.micronaut.test.annotation.MockBean;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.abondar.experimental.dao.PhoneFaunaRepository;
+import org.abondar.experimental.model.db.PhoneRecord;
+import org.abondar.experimental.model.web.PhoneRequest;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@MicronautTest
+public class PhoneFaunaServiceTest {
+
+  @Inject private PhoneFaunaService phoneFaunaService;
+
+  @Inject private PhoneFaunaRepository repository;
+
+  @MockBean(PhoneFaunaRepository.class)
+  PhoneFaunaRepository repository() {
+    return mock(PhoneFaunaRepository.class);
+  }
+
+  @Test
+  public void saveOrUpdateRecordTest() {
+    var req = new PhoneRequest("test", "9888");
+
+    var idCf = CompletableFuture.completedFuture(1L);
+    CompletableFuture<Void> saveCf = CompletableFuture.completedFuture(null);
+    when(repository.nextId()).thenReturn(idCf);
+    when(repository.saveOrUpdateRecord(any(PhoneRecord.class))).thenReturn(saveCf);
+
+    var res = phoneFaunaService.saveOrUpdateRecord(req);
+    assertEquals(1L, res.id());
+    assertEquals(req.name(), res.name());
+    assertEquals(req.phoneNumber(), res.phoneNumber());
+  }
+
+  @Test
+  public void findTest() {
+    var rec = new PhoneRecord(1L, "test", "test");
+
+    CompletableFuture<PhoneRecord> resCF = CompletableFuture.completedFuture(rec);
+    when(repository.find(rec.id())).thenReturn(resCF);
+
+    var res = phoneFaunaService.find(rec.id());
+    assertNotNull(res);
+  }
+
+  @Test
+  public void removeTest() {
+
+    CompletableFuture<Void> delCF = CompletableFuture.completedFuture(null);
+    when(repository.remove(1L)).thenReturn(delCF);
+
+    phoneFaunaService.remove(1L);
+    verify(repository, times(1)).remove(1L);
+  }
+}
