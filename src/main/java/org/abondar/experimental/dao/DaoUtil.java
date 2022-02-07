47815d2e9c13b8b7e157b7f6d13fb1752146a741
abondar24 <abondar1992@gmail.com>
2022-02-07 18:48:56 +0100

    minor update

--- src/main/java/org/abondar/experimental/dao/DaoUtil.java
+++ src/main/java/org/abondar/experimental/dao/DaoUtil.java
@@ -3,7 +3,7 @@ package org.abondar.experimental.dao;
 public class DaoUtil {
 
     public static final String DATA_VAL = "data";
-    public static final String COLLECTION_NAME = "Contacts";
+    public static final String COLLECTION_NAME = "Contact";
 
     private DaoUtil(){};
 }
