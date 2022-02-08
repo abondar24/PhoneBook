e0331e7d87be38b8fc315943804c0109a1fc225b
abondar24 <abondar1992@gmail.com>
2022-02-08 02:04:55 +0100

    fixed query strings

--- src/main/java/org/abondar/experimental/model/web/request/PhoneGraphqlRequest.java
+++ src/main/java/org/abondar/experimental/model/web/request/PhoneGraphqlRequest.java
@@ -3,16 +3,14 @@ package org.abondar.experimental.model.web.request;
 public class PhoneGraphqlRequest {
 
   public static final String saveRequest =
-      "{\"query\":\"mutation saveContact { createContact(data:{name:\"%s\" phoneNumber:\"%s\" }){_id}}\",\"variables\":{}}";
+      "{\"query\":\"mutation { createContact(data:{name:\\\"%s\\\" phoneNumber:\\\"%s\\\" }){_id}}\",\"variables\":{}}";
 
   public static final String updateRequest =
-      "{\"query\":\"mutation update { updateContact(id:\"%s\",data:{name:\"%s\"  phoneNumber:\"%s\"}){name phoneNumber}}\",\"variables\":{}}";
+      "{\"query\":\"mutation { updateContact(id:\\\"%s\\\",data:{name:\\\"%s\\\"  phoneNumber:\\\"%s\\\"}){name phoneNumber}}\",\"variables\":{}}";
 
-  public static final String findRequest =
-      "{\"query\":\"query find { findContactByID(id:\"%s\"){name, phoneNumber}}\",\"variables\":{}}";
-
+  public static final String findRequest = "{\"query\":\"query {findContactByID(id: \\\"%s\\\") {_id}}\",\"variables\":{}}";
   public static final String deleteRequest =
-      "{\"query\":\"mutation remove { deleteContact(id:\"%s\"){_id}}\",\"variables\":{}}";
+      "{\"query\":\"mutation { deleteContact(id:\\\"%s\\\"){_id}}\",\"variables\":{}}";
 
   private PhoneGraphqlRequest() {}
 }
